b690b4d6c085b3749f605fea1780c6e6366dd21d
FootNuggets22 <tutanekaitapiata@gmail.com>
2025-09-24 17:18:31 +1200

    push

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,7 +23,7 @@ const Hero = () => {
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.8, delay: 0.2 }}
           >
-            Kia Ora, I'm Tu<span className="highlight">Tu</span>
+            Kia Ora, I'm <span className="highlight">Tu</span>
           </motion.h1>
           
           <motion.p 
